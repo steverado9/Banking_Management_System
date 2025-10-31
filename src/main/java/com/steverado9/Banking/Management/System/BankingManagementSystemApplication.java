59ab42f19712aade80fc432ac728ab3437244abf
stephen <isaacsteverado9@gmail.com>
2025-10-31 16:55:44 +0100

    first commit

---
+++ src/main/java/com/steverado9/Banking/Management/System/BankingManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.steverado9.Banking.Management.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/steverado9/Banking/Management/System/BankingManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steverado9.Banking.Management.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
