6989dd1a70260db8e2a13240ca1b3d92141d653a
stephen <isaacsteverado9@gmail.com>
2025-11-19 03:10:39 +0100

    working on the transaction controller

--- src/main/java/com/steverado9/Banking/Management/System/controller/TransactionController.java
+++ src/main/java/com/steverado9/Banking/Management/System/controller/TransactionController.java
@@ -1,11 +1,23 @@
 package com.steverado9.Banking.Management.System.controller;
 
+import com.steverado9.Banking.Management.System.entity.Transaction;
 import com.steverado9.Banking.Management.System.service.TransactionService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TransactionController {
     private TransactionService transactionService;
 
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
 
+    @GetMapping("/deposit")
+    public String deposit(Model model) {
+        Transaction
+        model.addAttribute("amount", )
+    }
 }
